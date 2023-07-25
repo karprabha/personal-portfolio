876fd22de1ee2506fd1d383e2f279810efef8491
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-26 00:17:48 +0530

    add dropdown-menu for mobile

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+const menuButton: HTMLButtonElement = document.querySelector(".menu-btn");
+const navContainer: HTMLDivElement = document.querySelector(
+    "header > .container > nav"
+);
 
+const toggleMenu = (event: MouseEvent) => {
+    navContainer.classList.toggle("dropdown-menu");
+    navContainer.classList.toggle("show-menu");
+};
+
+menuButton.addEventListener("click", toggleMenu);
