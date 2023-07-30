3fee9f01a2d315568d675b2080831445fd5c6a08
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-31 03:15:36 +0530

    add dark-theme

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ import imageSlider from "./components/image-slider";
 
 dropdownMenu();
 imageSlider();
+
+const btn = document.getElementById("toggle-dark-mode");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+    } else {
+        document.body.classList.toggle("dark-theme");
+    }
+});
