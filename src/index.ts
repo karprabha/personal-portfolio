5dc3672c57d475d96ac279fa1e6e23b522c23df8
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-31 11:54:02 +0530

    add icon switch during theme toggle

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,20 @@ dropdownMenu();
 imageSlider();
 
 const btn = document.getElementById("toggle-dark-mode");
+const toggleThemeIcon = btn.querySelector(".icon");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
+if (prefersDarkScheme.matches) {
+    toggleThemeIcon.classList.toggle("sun-icon");
+    document.body.classList.toggle("dark-theme");
+} else {
+    toggleThemeIcon.classList.toggle("moon-icon");
+    document.body.classList.toggle("light-theme");
+}
+
 btn.addEventListener("click", function () {
-    if (prefersDarkScheme.matches) {
-        document.body.classList.toggle("light-theme");
-    } else {
-        document.body.classList.toggle("dark-theme");
-    }
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+    toggleThemeIcon.classList.toggle("sun-icon");
+    toggleThemeIcon.classList.toggle("moon-icon");
 });
